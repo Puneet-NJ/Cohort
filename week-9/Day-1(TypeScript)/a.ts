41ed85d432d163d4906e30e415c42b5e2ba78d61
Puneet NJ <lpuneetnj@gmail.com>
2024-05-14 00:29:35 +0530

    9.2 mid

---
+++ week-9/Day-1(TypeScript)/a.ts
@@ -0,0 +1,22 @@
+// function sum(a: number, b: number) {
+// 	let sum = a + b;
+// 	return sum;
+// }
+
+// const val = sum(1, 2);
+// console.log(val);
+
+// const above18 = (age: number): boolean => {
+// 	if (age > 18) return true;
+// 	else return false;
+// };
+
+// console.log(above18(17));
+
+const runFunc = (func: () => void) => {
+	setTimeout(func, 1000);
+};
+
+runFunc(() => {
+	console.log("Hello world");
+});
