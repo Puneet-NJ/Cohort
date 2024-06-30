4b127f97dc24a7a588bfd0a85e5d21b9ea23c4ed
Puneet NJ <lpuneetnj@gmail.com>
2024-06-30 18:53:29 +0530

    Re-do project

--- week-8/Todo(MERN)/Backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userRoute = require("./routes/user");
-const app = express();
-
-app.use(express.json());
-
-app.use("/api/v1/user", userRoute);
-
-app.listen(3000);
--- week-8/Todo(MERN)/Backend/schema.js
+++
@@ -1,9 +0,0 @@
-const zod = require("zod");
-
-const userSignupSchema = zod.object({
-	name: zod.string().min(3),
-	username: zod.string().min(3),
-	password: zod.string().min(7),
-});
-
-module.exports = { userSignupSchema };
