575829302afa504972e6e2c524ecd63f250b0561
Puneet NJ <lpuneetnj@gmail.com>
2024-07-26 13:20:03 +0530

    Did week-10 assignment but didn't test

---
+++ week-10/assignment/2-prisma-simple/src/db/user.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+/*
+ * Should insert into the users table
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function createUser(
+	username: string,
+	password: string,
+	name: string
+) {
+	const response = await prisma.user.create({
+		data: {
+			username,
+			password,
+			name,
+		},
+		select: {
+			username: true,
+			password: true,
+			name: true,
+		},
+	});
+
+	console.log(response);
+	return response;
+}
+
+/*
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function getUser(userId: number) {
+	const response = await prisma.user.findFirst({
+		where: {
+			id: userId,
+		},
+	});
+
+	console.log(response);
+	return response;
+}
