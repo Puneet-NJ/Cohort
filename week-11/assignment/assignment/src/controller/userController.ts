f3d5214a034ba15bdeb4db888c114e2e9ea60316
Puneet NJ <lpuneetnj@gmail.com>
2024-07-28 14:17:22 +0530

    Did signup route and user prisma accelerate in 11.1 assignment

--- week-11/assignment/assignment/src/controller/userController.ts
+++ week-11/assignment/assignment/src/controller/userController.ts
@@ -1,7 +1,8 @@
 import { Context } from "hono";
 import { signupSchema } from "../zod/user";
 import { STATUS_CODES } from "..";
-import { PrismaClient } from "@prisma/client";
+import { getPrisma } from "../prisma/prismaFunction";
+import { sign } from "hono/jwt";
 /*
 *
  - POST /users/signup - User registration.
@@ -10,30 +11,33 @@ Actions: Create a new user account. Perform validations and return a
 success message or error messages (e.g., email already in use, 
 password requirements not met).
 */
+
 export const signup = async (c: Context) => {
-	const prisma = new PrismaClient();
+	const prisma = getPrisma(c.env.DATABASE_URL);
 
 	const body = await c.req.json();
 
 	const validateSchema = signupSchema.safeParse(body);
-	// console.log(validateSchema);
 
 	if (!validateSchema.success)
 		return c.json({ msg: "Invalid inputs" }, STATUS_CODES.INVALID_INPUTS);
 
 	const userPresent = await prisma.users.findFirst({
-		where: {
-			email: body.email,
-		},
+		where: { OR: [{ username: body.username }, { email: body.email }] },
 	});
 	if (userPresent)
-		return c.json({ msg: "Email already exists" }, STATUS_CODES.INVALID_INPUTS);
+		return c.json(
+			{ msg: "Email / Username already exists" },
+			STATUS_CODES.INVALID_INPUTS
+		);
 
 	console.log(body);
 
-	// const data = await prisma.users.create({
-	// 	data: body,
-	// });
+	const data = await prisma.users.create({
+		data: body,
+	});
 
-	return c.json({ msg: "Testing" });
+	const token = await sign({ username: body.username }, c.env.JWT_SECRET);
+
+	return c.json({ token });
 };
