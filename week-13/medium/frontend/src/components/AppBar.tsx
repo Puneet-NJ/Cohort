474ed25e6c28a82ac9144ddc4ddae587a0ed1054
Puneet NJ <lpuneetnj@gmail.com>
2024-08-23 21:42:23 +0530

    Created a basic AppBar

---
+++ week-13/medium/frontend/src/components/AppBar.tsx
@@ -0,0 +1,15 @@
+import Avatar from "./Avatar";
+
+const AppBar = () => {
+	return (
+		<div className="flex justify-between px-16 text-lg py-4 border-b shadow-lg sticky top-0 z-10 bg-gray-300 mb-5">
+			<div className="font-medium">Medium</div>
+
+			<div>
+				<Avatar name="Puneet NJ" />
+			</div>
+		</div>
+	);
+};
+
+export default AppBar;
