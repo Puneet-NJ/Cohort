17910ef1f090c23c18b25b1b68f39e0abd5193e4
Puneet NJ <lpuneetnj@gmail.com>
2024-08-27 00:15:53 +0530

    Did 14.3 class upto "better fetches"

--- week-14/nextjs-backend/app/page.tsx
+++ week-14/nextjs-backend/app/page.tsx
@@ -1,12 +1,22 @@
-import axios from "axios";
+import { PrismaClient } from "@prisma/client";
+
+// You could do this, but you see that your nextjs server is sending request
+// from server to server i.e. inhouse
+// const getUserData = async () => {
+// 	const response = await axios({
+// 		method: "GET",
+// 		url: "http://localhost:3000/api/user",
+// 	});
+
+// 	return response.data;
+// };
+
+// Instead make the db call here itself
+const prisma = new PrismaClient();
 
 const getUserData = async () => {
-	const response = await axios({
-		method: "GET",
-		url: "http://localhost:3000/api/user",
-	});
-
-	return response.data;
+	const response = await prisma.user.findFirst({});
+	return response;
 };
 
 export default async function Home() {
@@ -16,9 +26,9 @@ export default async function Home() {
 		<div className="flex flex-col justify-center h-screen">
 			<div className="flex justify-center">
 				<div className="border p-8 rounded">
-					<div>Name: {userData?.name}</div>
+					<div>Name: {userData?.username}</div>
 
-					{userData?.email}
+					{userData?.password}
 				</div>
 			</div>
 		</div>
